ad74c08482c976da84552dc077d2dce0dd65f3ab
Kumail <kumailmn@gmail.com>
2017-09-12 15:07:47 -0400

    updated layout

--- app/src/main/java/com/kumailn/prayertime/Main2Activity.java
+++ app/src/main/java/com/kumailn/prayertime/Main2Activity.java
@@ -3,17 +3,14 @@ package com.kumailn.prayertime;
 import android.app.AlarmManager;
 import android.app.AlertDialog;
 import android.app.PendingIntent;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.IntegerRes;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
@@ -128,10 +125,12 @@ public class Main2Activity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(fajrSwitch.isChecked()){
                     saveSwitchAlarm("FajrAlarm", "true");
+                    onAlarmSwitchClick("Fajr");
                     Log.e(TAG, "Fajr Switch saved as on");
                 }
                 else {
                     saveSwitchAlarm("FajrAlarm", "false");
+                    try {dynamicFajrPendingIntent.cancel();} catch (Exception e) {e.printStackTrace();}
                     Log.e(TAG, "Switch saved as off");
                 }
             }
@@ -142,10 +141,12 @@ public class Main2Activity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(dhurSwitch.isChecked()){
                     saveSwitchAlarm("DhurAlarm", "true");
+                    onAlarmSwitchClick("Dhur");
                     Log.e(TAG, "dhur Switch saved as on");
                 }
                 else {
                     saveSwitchAlarm("DhurAlarm", "false");
+                    try {dynamicDhurPendingIntent.cancel();} catch (Exception e) {e.printStackTrace();}
                     Log.e(TAG, "Switch saved as off");
                 }
             }
@@ -156,10 +157,12 @@ public class Main2Activity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(asrSwitch.isChecked()){
                     saveSwitchAlarm("AsrAlarm", "true");
+                    onAlarmSwitchClick("Asr");
                     Log.e(TAG, "asr Switch saved as on");
                 }
                 else {
                     saveSwitchAlarm("AsrAlarm", "false");
+                    try {dynamicAsrPendingIntent.cancel();} catch (Exception e) {e.printStackTrace();}
                     Log.e(TAG, "Switch saved as off");
                 }
             }
@@ -170,10 +173,12 @@ public class Main2Activity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(maghribSwitch.isChecked()){
                     saveSwitchAlarm("MaghribAlarm", "true");
+                    onAlarmSwitchClick("Maghrib");
                     Log.e(TAG, "maghrib Switch saved as on");
                 }
                 else {
                     saveSwitchAlarm("MaghribAlarm", "false");
+                    try {dynamicMaghribPendingIntent.cancel();} catch (Exception e) {e.printStackTrace();}
                     Log.e(TAG, "Switch saved as off");
                 }
             }
@@ -184,16 +189,16 @@ public class Main2Activity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(asrSwitch.isChecked()){
                     saveSwitchAlarm("IshaAlarm", "true");
+                    onAlarmSwitchClick("Isha");
                     Log.e(TAG, "isha Switch saved as true");
                 }
                 else {
                     saveSwitchAlarm("IshaAlarm", "false");
+                    try {dynamicIshaPendingIntent.cancel();} catch (Exception e) {e.printStackTrace();}
                     Log.e(TAG, "Switch saved as off");
                 }
             }
         });
-
-
 
         Button aB = (Button)findViewById(R.id.alarmButton);
         Button offButton = (Button)findViewById(R.id.alarmOffButon);
@@ -633,6 +638,7 @@ public class Main2Activity extends AppCompatActivity {
         editor.putString(alarmName, state);
         editor.commit();
     }
+
     public String loadSwitchAlarm(String alarmName){
         SharedPreferences sharedPreferences = getSharedPreferences("myData", Context.MODE_PRIVATE);
         String myMethod = sharedPreferences.getString(alarmName, defaultMethod);
@@ -753,7 +759,6 @@ public class Main2Activity extends AppCompatActivity {
                 .show();
     }
 
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         //Configures menu (toolbar) button options
@@ -773,6 +778,170 @@ public class Main2Activity extends AppCompatActivity {
             return true;
         }*/
         return true;
+    }
+
+    public void onAlarmSwitchClick(String alarmName){
+        //Initialize dynamic alarm intents
+        Intent dynamicFajrIntent = new Intent(getApplicationContext(), prayerReceiver.class);
+        dynamicFajrIntent.putExtra("Prayer", "Fajr").putExtra("Type", "Dynamic");
+        Intent dynamicDhurIntent = new Intent(getApplicationContext(), prayerReceiver.class);
+        dynamicDhurIntent.putExtra("Prayer", "Dhur").putExtra("Type", "Dynamic");
+        Intent dynamicAsrIntent = new Intent(getApplicationContext(), prayerReceiver.class);
+        dynamicAsrIntent.putExtra("Prayer", "Asr").putExtra("Type", "Dynamic");
+        Intent dynamicMaghribIntent = new Intent(getApplicationContext(), prayerReceiver.class);
+        dynamicMaghribIntent.putExtra("Prayer", "Maghrib").putExtra("Type", "Dynamic");
+        Intent dynamicIshaIntent = new Intent(getApplicationContext(), prayerReceiver.class);
+        dynamicIshaIntent.putExtra("Prayer", "Isha").putExtra("Type", "Dynamic");
+
+        //Initialize pending intents
+        dynamicFajrPendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 101, dynamicFajrIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        dynamicDhurPendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 102, dynamicDhurIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        dynamicAsrPendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 103, dynamicAsrIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        dynamicMaghribPendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 104, dynamicMaghribIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        dynamicIshaPendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 105, dynamicIshaIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        //Initalize date format
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy/HH/mm");
+        Calendar cal2 = Calendar.getInstance();
+        String myday = (dateFormat.format(cal2.getTime()));
+
+        //Current Time variables
+        int currentDay = Integer.parseInt(myday.split("/")[0]);
+        int currentMonth = Integer.parseInt(myday.split("/")[1]);
+        int currentYear = Integer.parseInt(myday.split("/")[2]);
+        int currentHour = Integer.parseInt(myday.split("/")[3]);
+        int currentMin = Integer.parseInt(myday.split("/")[4]);
+
+        TimeZone tz1 = TimeZone.getDefault();
+        int offset = tz1.getRawOffset()/1000/60/60;
+
+        //Load longitute and latitute from saved data
+        double latitude = Double.parseDouble(loadLat());
+        double longitude = Double.parseDouble(loadLon());
+
+        //Load daylight-savings
+        Boolean myB = Boolean.valueOf(loadDaylight());
+        double timezone = offset;
+
+        //Adjust timezone based on daylight savings
+        if(myB == null){
+            timezone = offset;
+        }
+        else if(myB == false){
+            timezone = offset;
+        }
+        else{
+            timezone = timezone + 1;
+        }
+
+        //Initialize praytime object
+        PrayTime prayers = new PrayTime();
+        prayers.setTimeFormat(prayers.Time24);
+        prayers.setCalcMethod(loadDat());
+        prayers.setAsrJuristic(prayers.Shafii);
+        prayers.setAdjustHighLats(prayers.AngleBased);
+        int[] offsets = {0, 0, 0, 0, 0, 0, 0}; // {Fajr,Sunrise,Dhuhr,Asr,Sunset,Maghrib,Isha}
+        prayers.tune(offsets);
+
+        Date now = new Date();
+        Calendar todaysCalendar = Calendar.getInstance();
+        Calendar tomorrowCalendar = Calendar.getInstance();
+        tomorrowCalendar.set(currentYear, currentMonth - 1, currentDay + 1);
+        todaysCalendar.setTime(now);
+
+        //List of prayertimes for today
+        ArrayList<String> prayerTimes = prayers.getPrayerTimes(todaysCalendar, latitude, longitude, timezone);
+        //Prayer times for tomorrow
+        ArrayList<String> prayerTimes2 = prayers.getPrayerTimes(tomorrowCalendar, latitude, longitude, timezone);
+        //0 = Fajr, 1 = Sunrise, 2 = Dhur, 3 = Asr, 4 = Sunset, 5 = Maghrib, 6 = Isha
+        ArrayList<String> prayerNames = prayers.getTimeNames();
+
+        //Log to logCat
+        Log.e("geo: " + loadLat(), loadLon());
+        Log.e(prayerTimes.get(0) + " Today ", Integer.toString(loadDat()) + prayerNames.get(0));
+        Log.e(prayerNames.get(0), "0AAAAAAAAA");
+        Log.e(prayerNames.get(1), "1AAAAAAAAA");
+        Log.e(prayerNames.get(2), "2AAAAAAAAA");
+        Log.e(prayerNames.get(3), "3AAAAAAAAA");
+        Log.e(prayerNames.get(4), "4AAAAAAAAA");
+        Log.e(prayerNames.get(5), "5AAAAAAAAA");
+        Log.e(prayerNames.get(6), "6AAAAAAAAA");
+
+        //Initialize Gregorian Calendars from prayertime data
+        GregorianCalendar myCal = new GregorianCalendar(currentYear, currentMonth - 1, currentDay, currentHour, currentMin);
+        GregorianCalendar fajrCal = new GregorianCalendar(currentYear, currentMonth - 1, currentDay, Integer.parseInt(prayerTimes.get(0).split(":")[0]), Integer.parseInt(prayerTimes.get(0).split(":")[1]));
+        GregorianCalendar dhurCal = new GregorianCalendar(currentYear, currentMonth - 1, currentDay, Integer.parseInt(prayerTimes.get(2).split(":")[0]), Integer.parseInt(prayerTimes.get(2).split(":")[1]));
+        GregorianCalendar asrCal = new GregorianCalendar(currentYear, currentMonth - 1, currentDay, Integer.parseInt(prayerTimes.get(3).split(":")[0]), Integer.parseInt(prayerTimes.get(3).split(":")[1]));
+        GregorianCalendar maghribCal = new GregorianCalendar(currentYear, currentMonth - 1, currentDay, Integer.parseInt(prayerTimes.get(5).split(":")[0]), Integer.parseInt(prayerTimes.get(5).split(":")[1]));
+        GregorianCalendar ishaCal = new GregorianCalendar(currentYear, currentMonth - 1, currentDay, Integer.parseInt(prayerTimes.get(6).split(":")[0]), Integer.parseInt(prayerTimes.get(6).split(":")[1]));
+        //Tomorrow
+        GregorianCalendar fajrCal2 = new GregorianCalendar(currentYear, currentMonth - 1, currentDay + 1, Integer.parseInt(prayerTimes2.get(0).split(":")[0]), Integer.parseInt(prayerTimes2.get(0).split(":")[1]));
+        GregorianCalendar asrCal2 = new GregorianCalendar(currentYear, currentMonth - 1, currentDay + 1, Integer.parseInt(prayerTimes2.get(3).split(":")[0]), Integer.parseInt(prayerTimes2.get(3).split(":")[1]));
+        GregorianCalendar dhurCal2 = new GregorianCalendar(currentYear, currentMonth - 1, currentDay + 1, Integer.parseInt(prayerTimes2.get(2).split(":")[0]), Integer.parseInt(prayerTimes2.get(2).split(":")[1]));
+        GregorianCalendar maghribCal2 = new GregorianCalendar(currentYear, currentMonth - 1, currentDay + 1, Integer.parseInt(prayerTimes2.get(5).split(":")[0]), Integer.parseInt(prayerTimes2.get(5).split(":")[1]));
+        GregorianCalendar ishaCal2 = new GregorianCalendar(currentYear, currentMonth - 1, currentDay + 1, Integer.parseInt(prayerTimes2.get(6).split(":")[0]), Integer.parseInt(prayerTimes2.get(6).split(":")[1]));
+
+        //alarm_manager.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), pendingIntent2);
+        //Intent testIntent = new Intent(Main2Activity.this, prayerReceiver.class);
+        //testPendingIntent1 =  PendingIntent.getBroadcast(Main2Activity.this, 2, testIntent, 0);
+        //sendBroadcast(testIntent);
+        //TODO: Fix API compatibility
+        if(alarmName.equals("Fajr")){
+            if(System.currentTimeMillis() < fajrCal.getTimeInMillis()){
+                alarm_manager.setExact(AlarmManager.RTC_WAKEUP, fajrCal.getTimeInMillis(), dynamicFajrPendingIntent);
+                Log.e("FajrTodaySet:",String.valueOf(fajrCal.get(Calendar.YEAR)) + "/" + String.valueOf(fajrCal.get(Calendar.MONTH)+1)  + "/" +String.valueOf(fajrCal.get(Calendar.DAY_OF_MONTH)) + " " + String.valueOf(fajrCal.get(Calendar.HOUR)) + ":" + String.valueOf(fajrCal.get(Calendar.MINUTE)));
+            }
+            else{
+                alarm_manager.setExact(AlarmManager.RTC_WAKEUP, fajrCal2.getTimeInMillis(), dynamicFajrPendingIntent);
+                Log.e("FajrTomorrowSet:",String.valueOf(fajrCal2.get(Calendar.YEAR)) + "/" + String.valueOf(fajrCal2.get(Calendar.MONTH)+1)  + "/" +String.valueOf(fajrCal2.get(Calendar.DAY_OF_MONTH)) + " " + String.valueOf(fajrCal2.get(Calendar.HOUR)) + ":" + String.valueOf(fajrCal2.get(Calendar.MINUTE)));
+            }
+        }
+
+        else if(alarmName.equals("Dhur")){
+            if(System.currentTimeMillis() < dhurCal.getTimeInMillis()){
+                alarm_manager.setExact(AlarmManager.RTC_WAKEUP, dhurCal.getTimeInMillis(), dynamicDhurPendingIntent);
+                Log.e("DhurTodaySet:",String.valueOf(dhurCal.get(Calendar.YEAR)) + "/" + String.valueOf(dhurCal.get(Calendar.MONTH)+1) + "/" +String.valueOf(dhurCal.get(Calendar.DAY_OF_MONTH)) + " " + String.valueOf(dhurCal.get(Calendar.HOUR)) + ":" + String.valueOf(dhurCal.get(Calendar.MINUTE)));
+            }
+            else{
+                alarm_manager.setExact(AlarmManager.RTC_WAKEUP, dhurCal2.getTimeInMillis(), dynamicDhurPendingIntent);
+                Log.e("DhurTomorrowSet:",String.valueOf(dhurCal2.get(Calendar.YEAR)) + "/" + String.valueOf(dhurCal2.get(Calendar.MONTH)+1) + "/" +String.valueOf(dhurCal2.get(Calendar.DAY_OF_MONTH)) + " " + String.valueOf(dhurCal2.get(Calendar.HOUR)) + ":" + String.valueOf(dhurCal2.get(Calendar.MINUTE)));
+            }
+        }
+
+        else if(alarmName.equals("Asr")){
+            if(System.currentTimeMillis() < asrCal.getTimeInMillis()){
+                alarm_manager.setExact(AlarmManager.RTC_WAKEUP, asrCal.getTimeInMillis(), dynamicAsrPendingIntent);
+                Log.e("AsrTodaySet:",String.valueOf(asrCal.get(Calendar.YEAR)) + "/" + String.valueOf(asrCal.get(Calendar.MONTH)+1) + "/" +String.valueOf(asrCal.get(Calendar.DAY_OF_MONTH)) + " " + String.valueOf(asrCal.get(Calendar.HOUR)) + ":" + String.valueOf(asrCal.get(Calendar.MINUTE)));
+            }
+            else{
+                alarm_manager.setExact(AlarmManager.RTC_WAKEUP, asrCal2.getTimeInMillis(), dynamicAsrPendingIntent);
+                Log.e("AsrTomorrowSet:",String.valueOf(asrCal2.get(Calendar.YEAR)) + "/" + String.valueOf(asrCal2.get(Calendar.MONTH)+1) + "/" +String.valueOf(asrCal2.get(Calendar.DAY_OF_MONTH)) + " " + String.valueOf(asrCal2.get(Calendar.HOUR)) + ":" + String.valueOf(asrCal2.get(Calendar.MINUTE)));
+            }
+        }
+
+        else if(alarmName.equals("Maghrib")){
+            if(System.currentTimeMillis() < maghribCal.getTimeInMillis()){
+                alarm_manager.setExact(AlarmManager.RTC_WAKEUP, maghribCal.getTimeInMillis(), dynamicMaghribPendingIntent);
+                Log.e("MaghribTodaySet:",String.valueOf(maghribCal.get(Calendar.YEAR)) + "/" + String.valueOf(maghribCal.get(Calendar.MONTH)+1) + "/" +String.valueOf(maghribCal.get(Calendar.DAY_OF_MONTH)) + " " + String.valueOf(maghribCal.get(Calendar.HOUR)) + ":" + String.valueOf(maghribCal.get(Calendar.MINUTE)));
+            }
+            else{
+                alarm_manager.setExact(AlarmManager.RTC_WAKEUP, maghribCal2.getTimeInMillis(), dynamicMaghribPendingIntent);
+                Log.e("MaghribTomorrowSet:",String.valueOf(maghribCal2.get(Calendar.YEAR)) + "/" + String.valueOf(maghribCal2.get(Calendar.MONTH)+1) + "/" +String.valueOf(maghribCal2.get(Calendar.DAY_OF_MONTH)) + " " + String.valueOf(maghribCal2.get(Calendar.HOUR)) + ":" + String.valueOf(maghribCal2.get(Calendar.MINUTE)));
+            }
+        }
+
+        else if(alarmName.equals("Isha")){
+            if(System.currentTimeMillis() < ishaCal.getTimeInMillis()){
+                alarm_manager.setExact(AlarmManager.RTC_WAKEUP, ishaCal.getTimeInMillis(), dynamicIshaPendingIntent);
+                Log.e("IshaTodaySet:",String.valueOf(ishaCal.get(Calendar.YEAR)) + "/" + String.valueOf(ishaCal.get(Calendar.MONTH)+1) + "/" +String.valueOf(ishaCal.get(Calendar.DAY_OF_MONTH)) + " " + String.valueOf(ishaCal.get(Calendar.HOUR)) + ":" + String.valueOf(ishaCal.get(Calendar.MINUTE)));
+            }
+            else{
+                alarm_manager.setExact(AlarmManager.RTC_WAKEUP, ishaCal2.getTimeInMillis(), dynamicIshaPendingIntent);
+                Log.e("IshaTomorrowSet:",String.valueOf(ishaCal2.get(Calendar.YEAR)) + "/" + String.valueOf(ishaCal2.get(Calendar.MONTH)+1) + "/" +String.valueOf(ishaCal2.get(Calendar.DAY_OF_MONTH)) + " " + String.valueOf(ishaCal2.get(Calendar.HOUR)) + ":" + String.valueOf(ishaCal2.get(Calendar.MINUTE)));
+            }
+        }
+
+
     }
 
 
